369db310c17ca07bd567e1e69eca53589efe7c7c
tahbristol <tahbristol@gmail.com>
2018-05-19 15:11:36 -0400

    signup and login

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd65cee974fbbc376f5d5866c69dade58'
 
 posts = [
 	{
@@ -26,5 +29,15 @@ def hello():
 def about():
 	return render_template('about.html', title="About")
 
+@app.route("/register")
+def register():
+	form = RegistrationForm()
+	return render_template('register.html', form=form)
+	
+@app.route("/login")
+def login():
+	form = LoginForm()
+	return render_template('login.html', form=form)
+	
 if __name__ == '__main__':
 	app.run(debug=True)
