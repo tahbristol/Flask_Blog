07a2f841ae2b9b2fd81e85a175f550f5fce0a9f7
tahbristol <tahbristol@gmail.com>
2018-05-19 12:42:32 -0400

    templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+	{
+		'author': 'Tyler B.',
+		'title': 'Block Post 1',
+		'content': 'First post content',
+		'date_posted': 'May 19, 2018'
+	},
+	{
+		'author': 'Ali B.',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': 'May 19, 2018'
+	}
+]
+
+
 @app.route("/")
+@app.route("/home")
 def hello():
-    return "Hello World!"
+    return render_template('home.html', posts=posts)
+	
+@app.route("/about")
+def about():
+	return render_template('about.html', title="About")
+
+if __name__ == '__main__':
+	app.run(debug=True)
